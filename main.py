6cef170a0a612fdc650d46dddf90c109f6a664af
wbcastilho <welber.castilho@yahoo.com.br>
2022-08-23 21:46:47 -0300

    Alterado main para fazer o trayicon

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 from MainForm import MainForm
 import ttkbootstrap as ttk
+from pystray import MenuItem as item
+import pystray
+from PIL import Image
+import threading
+
+
+def quit_window(icon):
+    icon.stop()
+    app.destroy()
+
+
+def show_window(icon):
+    icon.stop()
+    app.after(0, app.deiconify())
+
+
+def hide_window():
+    app.withdraw()
+    image = Image.open("assets/favicon.ico")
+    menu = (item('Exibir', show_window), item('Sair', quit_window))
+    icon = pystray.Icon("name", image, "Monitor MQTT", menu)
+    icon.run()
+
+
+def hide_window_threading():
+    threading.Thread(daemon=True, target=hide_window).start()
 
 
 if __name__ == '__main__':
@@ -7,5 +33,7 @@ if __name__ == '__main__':
         title="Monitor MQTT",
     )
     MainForm(app)
+
+    app.protocol('WM_DELETE_WINDOW', hide_window_threading)
 
     app.mainloop()
